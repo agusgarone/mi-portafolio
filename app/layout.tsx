69933b2d6e304038391c6378b88a8ce242941371
agusgarone <agusgarone02@gmail.com>
2025-02-19 16:38:27 -0300

    Maquetacion de la pagina

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,21 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+const nunito = Nunito({
   subsets: ["latin"],
+  weight: ["300", "400", "700"], // Puedes elegir los pesos que necesites
+  variable: "--font-nunito", // Variable CSS
 });
 
 export const metadata: Metadata = {
@@ -23,12 +29,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className={nunito.variable}>
+      <body className={`antialiased`}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,39 @@
 "use client";
 
-import { useEffect } from "react";
-import { motion, useMotionValue, useSpring } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import { motion, useInView, useMotionValue, useSpring } from "framer-motion";
 import Link from "next/link";
 
 export default function Home() {
+  const [activeSection, setActiveSection] = useState("");
   const mouseX = useMotionValue(0);
   const mouseY = useMotionValue(0);
 
   const smoothX = useSpring(mouseX, { stiffness: 200, damping: 20 });
   const smoothY = useSpring(mouseY, { stiffness: 200, damping: 20 });
 
+  // Referencias para cada sección
+  const sobreMiRef = useRef(null);
+  const habilidadesRef = useRef(null);
+  const proyectosRef = useRef(null);
+
+  // Detectar cuándo una sección está visible
+  const isSobreMiInView = useInView(sobreMiRef, { amount: 0.6 });
+  const isHabilidadesInView = useInView(habilidadesRef, { amount: 0.6 });
+  const isProyectosInView = useInView(proyectosRef, { amount: 0.6 });
+
+  useEffect(() => {
+    if (isSobreMiInView) setActiveSection("sobre-mi");
+    else if (isHabilidadesInView) setActiveSection("habilidades");
+    else if (isProyectosInView) setActiveSection("proyectos");
+  }, [isSobreMiInView, isHabilidadesInView, isProyectosInView]);
+
+  useEffect(() => {});
+
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
-      mouseX.set(e.clientX);
-      mouseY.set(e.clientY);
+      mouseX.set(e.clientX - 64);
+      mouseY.set(e.clientY - 64);
     };
 
     window.addEventListener("mousemove", handleMouseMove);
@@ -24,57 +43,128 @@ export default function Home() {
   return (
     <div className="flex h-screen bg-gray-900 text-white">
       <motion.div
-        className="fixed w-8 h-8 bg-green-400 rounded-full opacity-50 pointer-events-none"
+        className="fixed w-32 h-32 bg-green-400 rounded-full opacity-30 pointer-events-none filter blur-3xl"
         style={{ x: smoothX, y: smoothY }}
-        animate={{ x: smoothX.get() - 16, y: smoothY.get() - 16 }}
+        animate={{ x: smoothX.get() - 64, y: smoothY.get() - 64 }}
       />
-      <div className="w-1/2 h-screen flex flex-col justify-center items-center top-0">
-        <div className="text-center">
-          <h1 className="text-5xl font-bold text-green-400">
-            Hola, soy Agustín Garone
-          </h1>
-          <p className="mt-4 text-lg text-gray-300">
-            Desarrollador Frontend & Mobile | React.js | React Native
-          </p>
-        </div>
-        <nav className="right-0 p-4 z-10">
-          <ul className="flex justify-center space-x-6 text-lg flex-col">
-            <li>
-              <Link href="#sobre-mi" className="hover:text-green-400">
-                Sobre Mí
-              </Link>
-            </li>
-            <li>
-              <Link href="#habilidades" className="hover:text-green-400">
-                Habilidades
-              </Link>
-            </li>
-            <li>
-              <Link href="#proyectos" className="hover:text-green-400">
-                Proyectos
-              </Link>
-            </li>
-            <li>
-              <Link href="#contacto" className="hover:text-green-400">
-                Contacto
-              </Link>
-            </li>
-          </ul>
-        </nav>
+      <div className="w-1/2 h-screen flex flex-col justify-between items-center p-24">
+        <section id="presentacion" className="flex flex-col gap-24">
+          <div className="text-start">
+            <h1 className="text-5xl font-bold text-green-400">
+              Agustín Garone
+            </h1>
+            <p className="mt-4 text-lg text-gray-300">
+              Desarrollador Frontend & Mobile | React.js | React Native
+            </p>
+          </div>
+          <nav className="right-0 p-4 z-10">
+            <ul className="flex justify-center text-lg flex-col gap-6">
+              {["sobre-mi", "habilidades", "proyectos"].map((section) => (
+                <li
+                  key={section}
+                  className="relative flex flex-row items-center"
+                >
+                  <motion.div
+                    className="w-full h-[3px] bg-green-400 rounded-full"
+                    layoutId="underline"
+                    initial={{ width: "5%" }}
+                    animate={{
+                      width: activeSection === section ? "15%" : "5%",
+                    }}
+                    transition={{ duration: 0.2 }}
+                  />
+                  <Link
+                    href={`#${section}`}
+                    className={`relative px-3 py-1 transition-all duration-300 ${
+                      activeSection === section
+                        ? "text-green-400 font-bold"
+                        : "text-white"
+                    }`}
+                  >
+                    {section.replace("-", " ").toUpperCase()}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </section>
+        <section id="contacto" className="flex flex-row gap-6 justify-start">
+          <p className="text-lg">🔹 github</p>
+          <p className="text-lg">🔹 linkedin</p>
+          <p className="text-lg">🔹 gmail</p>
+        </section>
       </div>
 
-      <div className="w-1/2 h-screen overflow-y-auto px-8 scroll-smooth">
-        <section id="sobre-mi" className="h-screen flex items-center">
-          <p className="text-lg">🔹 Sección 1: Algo sobre ti</p>
+      <div className="w-1/2 h-screen overflow-y-auto scroll-smooth pl-12">
+        <section
+          ref={sobreMiRef}
+          id="sobre-mi"
+          className="h-4/5 w-4/6 flex flex-col pt-24 mb-5"
+        >
+          <p className="text-lg">
+            Soy un desarrollador apasionado por la creación de interfaces de
+            usuario accesibles y perfectas en píxeles que combinan un diseño
+            bien pensado con una ingeniería sólida. Mi trabajo favorito se
+            encuentra en la intersección del diseño y el desarrollo, creando
+            experiencias que no sólo se ven muy bien, sino que están
+            meticulosamente construidas para el rendimiento y la usabilidad.
+          </p>
+          <br />
+          <p className="text-lg">
+            Actualmente, soy Ingeniero Semi Senior Front-End en Bewise.
+            Contribuyo a la creación y mantenimiento de componentes de interfaz
+            de usuario que potencian el front-end de los proyectos en los que
+            participo, asegurando que cada aplicacion cumpla con los estándares
+            de accesibilidad web y las mejores prácticas para ofrecer una
+            experiencia de usuario inclusiva.
+          </p>
+          <br />
+          <p className="text-lg">
+            En mi tiempo libre, suelo estar corriendo, en gimnasio, jugando o
+            mirando futbol apoyando a mi equipo Argentinos Juniors.
+          </p>
         </section>
-        <section id="habilidades" className="h-screen flex items-center">
-          <p className="text-lg">🔹 Sección 2: Tus habilidades</p>
+        <section
+          ref={habilidadesRef}
+          id="habilidades"
+          className="h-4/5 flex flex-col items-start"
+        >
+          <motion.div
+            className="relative w-full max-w-lg flex flex-row p-4 border border-gray-700 bg-gray-800 rounded-lg shadow-md transition-all duration-300 gap-10"
+            whileHover={{
+              scale: 1.05, // Se agranda ligeramente
+              boxShadow: "0px 10px 30px rgba(0, 255, 150, 0.2)", // Sombra verde
+              borderColor: "#00ff96", // Borde verde
+              backgroundColor: "#1f2937",
+            }}
+          >
+            <div id="tiempo-del-puesto">
+              <p className="tiempo">2021-presente</p>
+            </div>
+            <div
+              id="informacion-sobre-el-puesto"
+              className="flex flex-col gap-2"
+            >
+              <div id="puesto" className="titulo">
+                <p>Desarrollador Front-end en Bewise</p>
+              </div>
+              <div id="explicacion-del-puesto" className="descripcion">
+                <p>Explicacion del puesto</p>
+              </div>
+              <div id="stack-tecnologico-del-puesto">
+                <p className="tecnologia">Tecnologias que se usan</p>
+              </div>
+            </div>
+          </motion.div>
+          {/* <div id="Card" className="w-full flex flex-row p-4 rounded-l gap-10"> */}
+          {/* </div> */}
         </section>
-        <section id="proyectos" className="h-screen flex items-center">
+        <section
+          ref={proyectosRef}
+          id="proyectos"
+          className="h-4/5 w-4/6 flex items-center"
+        >
           <p className="text-lg">🔹 Sección 3: Tus proyectos</p>
-        </section>
-        <section id="contacto" className="h-screen flex items-center">
-          <p className="text-lg">🔹 Sección 4: Contacto</p>
         </section>
       </div>
     </div>
